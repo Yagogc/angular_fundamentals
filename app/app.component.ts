1cbbe61934ddde601a0808db5cef8751345c02ef
Yago <jagogc@gmail.com>
2017-10-15 01:27:25 +0100

    Trying templates

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,12 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
-  template: `
-    <div class="app">
-      Hello!
-    </div>
-  `
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
-
+  title: string;
+  name: string;
+  constructor() {
+    this.title = 'Ultimate Angular!'
+    this.name = ''
+  }
+  handleChange(value: string) {
+    this.name = value
+  }
 }
